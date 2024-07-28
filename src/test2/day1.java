f12c525979593348568eaf3f631facd2b31ae516
dineshcloudone <dinesh.cloudone@gmail.com>
2024-07-28 20:10:12 +0530

    first commit
    Signed-off-by: dineshcloudone <dinesh.cloudone@gmail.com>

---
+++ src/test2/day1.java
@@ -0,0 +1,16 @@
+package test2;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+
+	@Test
+	public void test_1() {
+		System.out.println("Test Method1");
+	}
+	
+	@Test
+	public void test_2() {
+		System.out.println("Test Method2");
+	}
+}
